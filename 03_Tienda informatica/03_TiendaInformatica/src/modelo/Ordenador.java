cf6af1a9635c691c87f3f78634e6e469a78ff0f3
Shaekhrom (Alejandro Barbacil) <111976003+Shaekhrom@users.noreply.github.com>
2024-04-09 16:08:47 +0200

    Delete 03_Tienda informatica directory

--- 03_Tienda informatica/03_TiendaInformatica/src/modelo/Ordenador.java
+++
@@ -1,8 +0,0 @@
-package modelo;
-
-public class Ordenador extends Producto {
-	private String procesador;
-	private String ram;
-	private String discoDuro;
-	private String gpu;
-}
--- 03_Tienda informatica/03_TiendaInformatica/src/modelo/Producto.java
+++
@@ -1,8 +0,0 @@
-package modelo;
-
-public abstract class Producto {
-	private int codigoUnico;
-	private String modelo;
-	private double precio;
-	private int stockActual;
-}
