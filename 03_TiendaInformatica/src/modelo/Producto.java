e8205643b3490fbd2ddcdebde5ee5f9c464fa6ef
Shaekhrom <shaekhrom@gmail.com>
2024-04-09 16:11:04 +0200

    Finalizado, diagrama dentro

---
+++ 03_TiendaInformatica/src/modelo/Producto.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public abstract class Producto {
+	private int codigoUnico;
+	private String modelo;
+	private double precio;
+	private int stockActual;
+}
---
+++ 03_TiendaInformatica/src/modelo/TelefonoMovil.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public class TelefonoMovil {
+	private String operador;
+}
